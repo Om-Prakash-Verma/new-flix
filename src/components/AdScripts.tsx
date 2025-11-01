274408ce0a94c56761545bbbd5dd61cb0ad0dbbe
Om Prakash Verma <flixwatch.site@gmail.com>
2025-11-01 19:27:41 +0530

    Update AdScripts.tsx

--- src/components/AdScripts.tsx
+++ src/components/AdScripts.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Script from 'next/script';
@@ -9,35 +8,36 @@ const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 export function AdScripts() {
   return (
     <>
-      < script > (function(s) {
-        s.dataset.zone = '9639504', s.src = 'https://bvtpk.com/tag.min.js'
-      })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script'))) < /script>
+      {/* 
+        Add your ad provider's script tags here.
+        You can use Next.js's <Script> component for optimized script loading.
+        
+        Example for Google AdSense:
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-YOUR_CLIENT_ID"
+          crossOrigin="anonymous"
+          strategy="lazyOnload" 
+        />
+      */}
 
-        {/* Google Analytics */}
-        {GA_TRACKING_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
+      {/* Google Analytics */}
+      {GA_TRACKING_ID && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', '${GA_TRACKING_ID}');
                 `}
-            </Script>
-          </>
-        )}
-      </>
-      );
+          </Script>
+        </>
+      )}
+    </>
+  );
 }
-
-      declare global {
-        interface Window {
-        _iezap ?: () => void;
-    _mdjlol?: () => void;
-  }
-}
-
